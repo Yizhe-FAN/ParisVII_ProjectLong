5f14b333b2d2f15cf473b7a33e226aab8aa66ff5
Fan <fanyizhe3015@gmail.com>
2016-10-03 17:58:47 +0200

    tester moteur

--- src/move/Moteur.java
+++ src/move/Moteur.java
@@ -1,13 +1,51 @@
 package move;
 
 import lejos.nxt.Button;
+import lejos.nxt.LCD;
+import lejos.nxt.Motor;
 
 public class Moteur {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello Mindstorms");
+		Motor.A.forward();
+		LCD.drawString("forward", 0, 1);
 		Button.waitForAnyPress();
+		Motor.A.backward();
+		LCD.drawString("backward", 0, 2);
+		Button.waitForAnyPress();
+		Motor.A.stop();
+		Button.waitForAnyPress();
+		
+		Motor.A.resetTachoCount();
+		int angle = Motor.A.getTachoCount();
+		LCD.drawInt(angle, 0, 3);
+		LCD.drawInt(Motor.A.getSpeed(), 4, 3);
+		Button.waitForAnyPress();
+		
+		Motor.A.setSpeed(900);
+		LCD.drawInt(Motor.A.getSpeed(), 8, 3);
+		Button.waitForAnyPress();
+		
+
+		Motor.A.forward();		
+		while(angle < 1080) 
+		{
+			angle=Motor.A.getTachoCount();
+		}
+		LCD.drawInt(angle, 0, 4);
+		
+		Motor.A.resetTachoCount();
+		Motor.A.flt();
+		//Motor.A.stop();
+		
+		Button.waitForAnyPress();
+		
+		angle = Motor.A.getTachoCount();
+		LCD.drawInt(angle, 0, 5);
+		Button.waitForAnyPress();
+		
 	}
 
 }
