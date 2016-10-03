24c493f2ef78db82d90b9b34c2d53b79a7fe10fa
Fan <fanyizhe3015@gmail.com>
2016-10-03 13:36:03 +0200

    First commit

---
+++ src/move/Moteur.java
@@ -0,0 +1,13 @@
+package move;
+
+import lejos.nxt.Button;
+
+public class Moteur {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello Mindstorms");
+		Button.waitForAnyPress();
+	}
+
+}
