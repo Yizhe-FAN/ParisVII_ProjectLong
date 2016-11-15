1cfe99e8ef7a0d5f27b6765544266518c2ada686
chensi <moonichel@gmail.com>
2016-11-15 14:35:24 +0100

    "add"

---
+++ src/move/ColorState.java
@@ -0,0 +1,27 @@
+package move;
+
+public class ColorState {
+
+private int red;
+private int green;
+private int blue;
+
+public ColorState(int r, int g, int b){
+	this.red = r;
+	this.green = g;
+	this.blue = b;
+}
+
+public int getRed(){
+	return red;
+}
+
+public int getGreen(){
+	return green;
+}
+
+public int getBlue(){
+	return blue;
+}
+	
+}
