4aa3a0ea02806cf6906f1147dcd0b7380f100c20
chensi <moonichel@gmail.com>
2016-11-24 17:18:55 +0100

    a

--- src/move/RunRobot.java
+++ src/move/RunRobot.java
@@ -1,14 +1,34 @@
 package move;
 
+import java.io.File;
+
 import lejos.nxt.ColorSensor;
+import lejos.nxt.LCD;
 
 public class RunRobot {
 	
 	public static void main(String args[]){
-		ControlColorSensor mColorSensor = new ControlColorSensor();
-		ColorSensor cs = mColorSensor.setColorSenor();
-		mColorSensor.colorReader(cs);
-		mColorSensor.colorChecker(cs);
+		File file =new File("ListBackUp.txt");
+		if(!file.exists()){
+			LCD.drawString("Initialise File!", 0, 6);
+			ControlColorSensor mColorSensor = new ControlColorSensor();
+			ColorSensor cs = mColorSensor.setColorSenor();
+			FileHandler f = new FileHandler();
+			mColorSensor.colorReader(cs);
+			mColorSensor.colorChecker(cs);
+			f.writeInFile();
+		}else{
+			LCD.drawString("Load and use File!", 0, 6);
+			FileHandler f = new FileHandler();
+			f.readInList("ListBackUp.txt");
+			ControlColorSensor mColorSensor = new ControlColorSensor();
+			ColorSensor cs = mColorSensor.setColorSenor();
+			mColorSensor.colorReader(cs);
+			mColorSensor.colorChecker(cs);
+			f.writeInFile();
+			
+		}
+		
 	}
 
 }
