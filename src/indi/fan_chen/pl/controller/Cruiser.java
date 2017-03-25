55561ff2585e81cc8f70f51173b61f85d67465a3
chensi <moonichel@gmail.com>
2017-03-25 22:47:10 +0100

    cruiser

--- src/indi/fan_chen/pl/controller/Cruiser.java
+++ src/indi/fan_chen/pl/controller/Cruiser.java
@@ -9,7 +9,7 @@ public class Cruiser extends Thread{
 	double kp = 1.2;
 	double ki = 0.0008;
 	double kd = 5;
-	int median = 50;
+	int median = 5;
 	int value = 0;
 	int error = 0;
 	int integral = 0;
@@ -25,7 +25,7 @@ public class Cruiser extends Thread{
 	
 	NXTMotor ma = new NXTMotor(MotorPort.A);
 	NXTMotor mb = new NXTMotor(MotorPort.B);
-	
+
 	public Cruiser(ControlColorSensor sensor){
 		sensor1 = sensor;
 	}
@@ -39,14 +39,33 @@ public class Cruiser extends Thread{
 			
 			correction = kp*error + ki*integral + kd*derivative;
 			
-			aPower = 20	- correction;
-			bPower = 20	+ correction;
+			LCD.clear(2);
+			LCD.drawString("Error " + correction, 0, 2);
+			LCD.clear(3);
+			LCD.drawString("Correction " + correction, 0, 3);
 			
-			ma.setPower(new Double(aPower).intValue());
-			ma.forward();
-			mb.setPower(new Double(bPower).intValue());
-			mb.forward();
+			if(correction > 50){
+				correction = 50;
+			}else if(correction < -50){
+				correction = -50;
+			}
 			
+			aPower = 50 - correction;
+			bPower = 50 + correction;
+			
+			if(error< -45){
+				ma.setPower(40);
+				ma.forward();
+				mb.setPower(40);
+				mb.forward();
+			}else{
+				ma.setPower(new Double(aPower).intValue());
+				ma.forward();
+				mb.setPower(new Double(bPower).intValue());
+				mb.forward();
+			}
+			
+					
 			lastError = error;
 		}
 	}
@@ -55,7 +74,7 @@ public class Cruiser extends Thread{
 		int res;
 		color1 = 0;
 		color2 = 0;
-		for(int i = 0; i < 20; ++i){
+		for(int i = 0; i < 10; ++i){
 			res = sensor1.colorChecker();
 			if(res == 1){
 				color1++;
@@ -63,7 +82,7 @@ public class Cruiser extends Thread{
 				color2++;
 			}
 		}
-		return (color1/(color1+color2))*100;//return color of background
+		return (color1/(color1+color2))*10;//return color of background
 	}
 
 }
