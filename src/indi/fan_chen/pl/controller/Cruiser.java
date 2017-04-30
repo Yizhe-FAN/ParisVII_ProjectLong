6d3cd418f7f0f1a4f13282b21e4e1ef7637bb43c
chensi <moonichel@gmail.com>
2017-04-30 19:54:57 +0200

    clean code

--- src/indi/fan_chen/pl/controller/Cruiser.java
+++ src/indi/fan_chen/pl/controller/Cruiser.java
@@ -1,149 +1,16 @@
 package indi.fan_chen.pl.controller;
 
-import indi.fan_chen.pl.model.ColorType;
-import indi.fan_chen.pl.model.RgbState;
 import lejos.nxt.*;
 
 public class Cruiser extends Thread{
-	
-	private ControlColorSensor sensor1;
-	private ColorSensor colorSensor;
-	int rInter,gInter,bInter;
-	
-	double kp;
-	double ki = -30;
-	//double kd = 5;
-	//int derivative = 0;
-	//int lastError = 0;
-	//double correction = 0;
-	
-	//double leftPower;
-	//double rightPower;
-	
-	RgbState background;
-	RgbState line;
-	
-	NXTMotor ma = new NXTMotor(MotorPort.A);
-	NXTMotor mb = new NXTMotor(MotorPort.B);
-	
-	double error = 0;
-	double offSet;
-	double powerStandard = 60;
-	double powerVal = 50;
-	double integral = 0;
 
 	public Cruiser(ControlColorSensor sensor){
-		sensor1 = sensor;
-		colorSensor = new ColorSensor(SensorPort.getInstance(0));
-		background = new RgbState(0);
-		line = new RgbState(0);
-
-		getRgbAvg(0, background);
-		getRgbAvg(1, line);
 		
-		double corBase = calculCor(background.r, background.g, background.b, line);
-		offSet = (corBase + 1) / 2;
-		
-		kp = 1 / (corBase - offSet);
 	}
 	
 	public void run(){
 		while(!Button.ESCAPE.isDown()){
-			ColorSensor.Color vals = colorSensor.getColor();
-			
-			double newCor = calculCor(vals.getRed(), vals.getGreen(), vals.getBlue(), line);
-			
-			double newError = newCor - offSet;
-			
-			if ( (newError * error) < 0 ){
-				integral = 0;
-			}
-			error = newError;
-			
-			integral += error;
-			
-			double turn = powerVal * kp * error + ki * integral;
-			
-			double powerA = powerStandard - turn;
-			double powerB = powerStandard + turn;
-			
-			if(powerA > 0){
-				ma.setPower(new Double(powerA).intValue());
-				ma.forward();
-			}
-			else {
-				powerA = powerA*(-1);
-				ma.stop();
-				ma.backward();
-				ma.setPower(new Double(powerA).intValue());
-				
-			}
-			
-			if(powerB > 0){
-				mb.setPower(new Double(powerB).intValue());
-				mb.forward();
-			}
-			else {
-				powerB = powerB*(-1);
-				mb.stop();
-				mb.backward();
-				mb.setPower(new Double(powerB).intValue());
-				
-			}
-			
-			//error = getError(median, vals.getRed(), vals.getGreen(), vals.getBlue());
-			//LCD.clear(4);
-			//LCD.drawString("R"+vals.getRed()+"G"+vals.getGreen()+"B"+vals.getBlue(), 0, 4);
-			//integral += error;
-			//derivative = error - lastError;
-			
-			//correction = kp*error + ki*integral + kd*derivative;
-			
-			//LCD.clear(2);
-			//LCD.drawString("Error " + error, 0, 2);
-			//LCD.clear(3);
-			//LCD.drawString("Correction " + correction, 0, 3);
-			
-			/*if(correction > 50){
-				correction = 50;
-			}else if(correction < -50){
-				correction = -50;
-			}
-			
-			leftPower = 50 - correction;
-			rightPower = 50 + correction;
-			
-			if(error< -45){
-				ma.setPower(40);
-				ma.forward();
-				mb.setPower(40);
-				mb.forward();
-			}else{
-				ma.setPower(new Double(leftPower).intValue());
-				ma.forward();
-				mb.setPower(new Double(rightPower).intValue());
-				mb.forward();
-			}*/
-						
-			//lastError = error;
 		}
-	}
-	
-	private double calculCor(int r, int g, int b, RgbState base){
-		double newColor = Math.sqrt(r*r + g*g + b*b);
-		double baseColor = Math.sqrt(base.r*base.r + base.g*base.g + base.b*base.b);
-		
-		double colorMix = r * base.r + g * base.g + b * base.b;  
-	    double cor = colorMix / (newColor * baseColor);
-		return cor;
-	}
-	
-	
-	private void getRgbAvg(int index, RgbState state){
-		ColorType tempCT = sensor1.colorTypeList.get(index);
-		state.r = tempCT.rgbAvg.r;
-		state.g = tempCT.rgbAvg.g;
-		state.b = tempCT.rgbAvg.b;
 	}
 
 }
--- src/indi/fan_chen/pl/controller/FollowLine.java
+++ src/indi/fan_chen/pl/controller/FollowLine.java
@@ -12,7 +12,7 @@ import lejos.nxt.NXTMotor;
 public class FollowLine {
 
 	static final int WHITE = 1;
-	static final int BLACK = 3;
+	static final int STOP = 3;
 	static final int LEFT = 0;
 	static final int RIGHT = 1;
 	static int mline = 1;
@@ -57,7 +57,7 @@ public class FollowLine {
 			ColorSensor.Color vals = sensor1.colorSensor.getColor();
 			res = sensor1.colorChecker();
 			
-			if(res == BLACK) {
+			if(res == STOP) {
 				ma.stop();
 				mb.stop();
 				break;
@@ -125,12 +125,6 @@ public class FollowLine {
 		return cor;
 	}
 	
-	/*public void runFollowLineMode1(){
-		LCD.clear();
-		LCD.drawString("---Follow Line---", 0, 0);
-		init();
-	}*/
-
 	public void runFollowLineMode2(){
 		LCD.clear();
 		LCD.drawString("---Follow Line---", 0, 0);
@@ -207,145 +201,11 @@ public class FollowLine {
 		Thread curiser = new Thread(new Cruiser(sensor1));
 		curiser.start();
 	}*/
-
-	/*
-	public void runFollowLineMode1(){
-		LCD.clear();
-		LCD.drawString("---Follow Line---", 0, 0);
-
-		int lastTime = LEFT;
-		Moteur mMoteur = new Moteur();
-		int res;
-		mMoteur.mForward(100);
-
-		while(!Button.ESCAPE.isDown()){
-			res = sensor1.colorChecker();
-			LCD.clear(3);
-			LCD.drawString("Color "+res+" Detected", 0, 3);
-			LCD.refresh();
-
-			while((!Button.ESCAPE.isDown())&&(res == WHITE)){
-				if(lastTime == LEFT){
-					//turn right
-					Motor.A.setSpeed(60);
-					Motor.B.setSpeed(260);
-				}
-				else{
-					//turn left
-					Motor.A.setSpeed(260);
-					Motor.B.setSpeed(60);
-				}
-				res = sensor1.colorChecker();
-			}
-
-			while((!Button.ESCAPE.isDown())&&(res != WHITE)){
-				if(lastTime == LEFT){
-					Motor.A.setSpeed(60);
-					Motor.B.setSpeed(260);
-				}
-				else{
-					Motor.A.setSpeed(260);
-					Motor.B.setSpeed(60);
-				}
-				res = sensor1.colorChecker();
-			}
-
-			lastTime = Math.abs(lastTime-1);
-
-		}
-	}*/
-
-	/*public void runFollowLineMode3(ControlColorSensor sensor1, ControlColorSensor sensor2){
-		LCD.clear();
-		LCD.drawString("---Follow Line---", 0, 0);
-		int res1, res2, times = 1;
-		int mTimerOK = 1, mWaitOK = 1;
-		int speed = 400;
-
-
-		Wait mWait = new Wait(100);
-		Timer mTimer = new Timer(50);
-
-		Moteur mMoteur = new Moteur();
-		mMoteur.mForward(speed);
-
-		while(!Button.ESCAPE.isDown()){
-			res1 = sensor1.colorChecker();
-			res2 = sensor2.colorChecker();
-			LCD.clear(3);
-			LCD.clear(4);
-			LCD.drawString("Sensor1 "+res1+" Detected", 0, 3);
-			LCD.drawString("Sensor2 "+res2+" Detected", 0, 4);
-			LCD.refresh();
-
-			while((!Button.ESCAPE.isDown()) && (res1 != 1) && (res2 == 1)){
-				//turn right
-				if(mTimerOK == 1){
-					new Thread(mTimer).start();
-					mTimerOK = 0;
-				}
-				if(mline == 1){
-					Motor.A.setSpeed(speed - 100);
-					Motor.B.setSpeed(speed);
-				}else{
-					speed = 400;
-					Motor.A.setSpeed(speed - 100*times);
-					Motor.B.setSpeed(speed);
-					if(mWaitOK == 1)
-						new Thread(mWait).start();
-					if(addTime == 1){
-						if((speed-100*times) > 0)
-							times++;
-						addTime = 0;
-						mWaitOK = 1;
-					}
-				}
-				res1 = sensor1.colorChecker();
-				res2 = sensor2.colorChecker();
-			}
-			mMoteur.mForward(speed);
-			times = 1;
-			mline = 1;
-			mTimerOK = 1;
-			mTimer.stopRequest();
-
-			while((!Button.ESCAPE.isDown()) && (res1 == 1) && (res2 != 1)){
-				//turn left
-				if(mTimerOK == 1){
-					new Thread(mTimer).start();
-					mTimerOK = 0;
-				}
-				if(mline == 1){
-					Motor.A.setSpeed(speed);
-					Motor.B.setSpeed(speed - 100);
-				}else{
-					speed = 400;
-					Motor.A.setSpeed(speed);
-					Motor.B.setSpeed(speed - 100*times);
-					if(mWaitOK == 1)
-						new Thread(mWait).start();
-					if(addTime == 1){
-						if((speed-100*times) > 0)
-							times++;
-						addTime = 0;
-						mWaitOK = 1;
-					}
-				}
-				res1 = sensor1.colorChecker();
-				res2 = sensor2.colorChecker();
-			}
-
-			mMoteur.mForward(speed);
-			times = 1;
-			mline = 1;
-			mTimerOK = 1;
-			mTimer.stopRequest();
-
-			if(speed < 600){
-				speed+=50;
-			}
-		}
-
+	
+	/*public void runFollowLineMode1(){
+	LCD.clear();
+	LCD.drawString("---Follow Line---", 0, 0);
+	init();
 	}*/
 
 }
